d2397324461cb02d2a176b7314b1bd88aec6fa66
beliayam <beliayamcom@gmail.com>
2022-01-14 14:57:55 +0700

    first build

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+ 
+$('#us2').locationpicker({
+   location: {latitude: -6.200000, longitude: 106.816666},   
+   radius: 0,
+   inputBinding: {
+      latitudeInput: $('#lat'),
+      longitudeInput: $('#lng'),
+      locationNameInput: $('#location')
+   },
+   enableAutocomplete: true,
+   onchanged: function(currentLocation, radius, isMarkerDropped) {
+      alert("Location changed. New location (" + currentLocation.latitude + ", " + currentLocation.longitude + ")");
+    }
+});
+ 
+ 
+});
